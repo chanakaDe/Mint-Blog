ecd921aedbaad48704e926053c5b86323ac2afd3
chanaka_de_silva <chanaka@srilanka>
2015-10-11 11:20:13 +0530

    Finalized basic functions of signup , login, add new story , get stries by user and get all stories.

--- app/controller/AddNewPostController.js
+++ app/controller/AddNewPostController.js
@@ -4,8 +4,8 @@
 function AddNewPostController($scope, storyService) {
 
     $scope.previewContent = function () {
-        $scope.title = $scope.story.postTitle;
-        $scope.content = $scope.story.postContent;
+        $scope.title = $scope.story.title;
+        $scope.content = $scope.story.content;
         $scope.showPreview = true;
     };
 
@@ -15,12 +15,14 @@ function AddNewPostController($scope, storyService) {
     $scope.makeNewStory = function () {
 
         var story = $scope.story;
+        console.log(story);
 
         storyService.createStory(story).success(function (data) {
             console.log(data);
+            $.notify("NEW STORY ADDED SUCCESSFULLY", "success");
             $scope.story = {};
             $scope.message = data.message;
-            $scope.stories.push(data);
+            //$scope.stories.push(data);
         });
     };
 
